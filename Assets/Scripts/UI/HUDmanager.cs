7d9255d212b15b3f8c15c2a523ceb7bfe4ba3bed
CyrusTeamDevelopment <87054863+CyrusTeamDevelopment@users.noreply.github.com>
2022-06-12 14:50:55 +1200

    Added show UI key

--- Assets/Scripts/UI/HUDmanager.cs
+++ Assets/Scripts/UI/HUDmanager.cs
@@ -69,6 +69,11 @@ public class HUDmanager : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            showFullUI();
+        }
+
         MoneyText.text = "$" + currentMoney.ToString();
 
         if (currentMoney < currentMAXmoney)
