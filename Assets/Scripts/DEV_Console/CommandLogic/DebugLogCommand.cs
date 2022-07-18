e04133f9b57f108554d0d2c544ee0feeea53056c
CyrusTeamDevelopment <87054863+CyrusTeamDevelopment@users.noreply.github.com>
2022-07-18 14:32:20 +1200

    Developer console

---
+++ Assets/Scripts/DEV_Console/CommandLogic/DebugLogCommand.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLogCommand", menuName = "Utilities/DeveloperCommands/DebugLogCommand")]
+public class DebugLogCommand : ConsoleCommand
+{
+    public override bool Process(string[] args)
+    {
+        string logText = string.Join(" ", args);
+
+        Debug.Log(logText);
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/DEV_Console/DeveloperConsoleBehaviour.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DeveloperConsoleBehaviour : MonoBehaviour
+{
+    [SerializeField] private string prefix = string.Empty;
+    [SerializeField] private ConsoleCommand[] commands = new ConsoleCommand[0];
+
+    [Header("UI")]
+    [SerializeField] private GameObject uiCanvas = null;
+    [SerializeField] private TMP_InputField inputField = null;
+    [SerializeField] private TextMeshProUGUI logText;
+
+    private static DeveloperConsoleBehaviour instance;
+
+    private DeveloperConsole developerConsole;
+
+    private DeveloperConsole DeveloperConsole
+    {
+        get
+        {
+            if(developerConsole != null) { return developerConsole; }
+            return developerConsole = new DeveloperConsole(prefix, commands);
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ToggleDevConsole()
+    {
+        if (uiCanvas.activeSelf)
+        {
+            uiCanvas.SetActive(false);
+            PlayerMove.Singleton.canMove = true;
+        }
+        else
+        {
+            uiCanvas.SetActive(true);
+            inputField.ActivateInputField();
+            PlayerMove.Singleton.canMove = false;
+        }
+    }
+
+    public void ProcessCommand(string inputValue)
+    {
+        DeveloperConsole.ProcessCommand(inputValue);
+
+        inputField.text = string.Empty;
+        inputField.ActivateInputField();
+
+        logText.text = logText.text + "<br>" + inputValue;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backslash))
+        {
+            ToggleDevConsole();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Return) && inputField.text != string.Empty)
+        {
+            ProcessCommand(inputField.text);
+        }
+    }
+
+}
